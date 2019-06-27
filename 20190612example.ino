7f74b754ffde704d7941ad166ca10d376fbcd5fd
happyg920624 <51997834+happyg920624@users.noreply.github.com>
2019-06-27 15:58:26 +0800

    Add files via upload

---
+++ 20190612example.ino
@@ -0,0 +1,38 @@
+/*
+ * Generated using BlocklyDuino:
+ *
+ * https://github.com/MediaTek-Labs/BlocklyDuino-for-LinkIt
+ *
+ * Date: Wed, 12 Jun 2019 12:27:49 GMT
+ */
+
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd_i2c(0x3F);
+
+#include <DHT.h>
+
+DHT dht11_p2(2, DHT11);
+
+void setup()
+{
+  lcd_i2c.begin(16, 2);
+
+  dht11_p2.begin();
+  Serial.begin(9600);
+
+}
+
+
+void loop()
+{
+  Serial.println(dht11_p2.readHumidity());
+  delay(200);
+  Serial.println(dht11_p2.readTemperature());
+  delay(200);
+if (false) {
+
+} else {
+
+}
+
+}
